919649f79342c3261084c75435f81725a23fb38b
A L NARENDRA KUMAR <46441621+athkurinarendra@users.noreply.github.com>
2021-07-06 19:03:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+
+import pickle
+import numpy as np
+from flask import Flask, render_template, request
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/", methods = ['POST'])
+def predict():
+
+    age_group = request.form['age_group']
+    total_deaths = request.form['total_deaths']
+    data = [[age_group,total_deaths]]
+    out = model.predict(data)
+
+    return "Covid-19 deaths:"+str(np.round((out[0])))
+
+if __name__ == "__main__":
+
+    app.run()
+   
+    
+   
